40db462f8988b4dedabb8c4084e69739dd2215da
Juan Vanegas <jdvanegasm@udistrital.edu.co>
2024-11-17 17:56:54 -0500

    coink_api integrity review

--- coink_api/Program.cs
+++ coink_api/Program.cs
@@ -3,10 +3,12 @@ using coink_api.data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// db configs
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
